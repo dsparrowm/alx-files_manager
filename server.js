717ca84499058a26e48918cee2a8efd062c5d055
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-09-06 12:44:26 -0700

    fixed linting errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const express = require("express");
-const router = require("./routes/index");
+const express = require('express');
+const router = require('./routes/index');
 
 const server = express();
 const PORT = process.env.PORT ? process.env.PORT : 5000;
@@ -9,6 +9,4 @@ const PORT = process.env.PORT ? process.env.PORT : 5000;
 server.use(express.json());
 server.use(router);
 
-server.listen(PORT, () =>
-  console.log(`The server is running on port: ${PORT}`)
-);
+server.listen(PORT, () => console.log(`The server is running on port: ${PORT}`));
